2c4cb5af7615c11b72b92b3a361f8de4116ca74b
electriccheffer <electriccheff@gmail.com>
2026-07-09 05:31:36 -0500

    GENERATED first component for status api

---
+++ frontend/RESTFULPiFrontend/src/app/components/status/status.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-status',
+  imports: [],
+  templateUrl: './status.html',
+  styleUrl: './status.css',
+})
+export class Status {}
