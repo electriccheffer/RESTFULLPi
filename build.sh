10ddef743e4fab5705cc0a04feccd90650a31729
electriccheffer <electriccheff@gmail.com>
2026-07-28 23:05:18 -0500

    Build process complete for front end

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+## Run frontend tests 
+
+## Build the Angular app 
+
+## Run the backend tests 
+
+## Compile the go binary with the angular application 
+
+## Transfer the files to the Raspberry Pi 
+
+## Restart the pi's service 
+
+## Switch back network
