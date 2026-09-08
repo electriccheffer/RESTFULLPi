09c5b791eae13110e33e481e9ee64514d533f995
electriccheffer <electriccheff@gmail.com>
2026-09-08 15:49:23 -0500

    REIMPLEMENTED wrapper for os level operations

--- backend/internal/file_operations/file_operations.go
+++ backend/internal/file_operations/file_operations.go
@@ -2,7 +2,49 @@ package file_operations
 
 import "io/fs"
 import "os"
+import "io"
 
+
+type FileHandle interface {
+
+	io.ReadWriteCloser	
+}
+
+type Opener interface{
+	
+	Open(path string)(FileHandle,error)
+	Create(path string)(FileHandle,error)
+}
+
+
+type FileOpener struct{}
+
+func NewFileOpener()*FileOpener{
+		
+	fo := &FileOpener{}
+	return fo
+}
+
+
+func (fo *FileOpener) Open(path string)(FileHandle,error){
+	
+	file,err := os.Open(path)
+	if err != nil {
+
+		return nil,err
+	}
+	return file,nil
+	
+}
+
+func (fo *FileOpener) Create(path string)(FileHandle,error){
+
+	file,err := os.Create(path)
+	if err != nil{	
+		return nil, err
+	}
+	return file,nil
+}
 
 type DirectoryRead struct{
 	
